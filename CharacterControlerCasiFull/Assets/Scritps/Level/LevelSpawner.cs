b8dc1a407a9fe19d56204a5bdce8793458684a49
jitox <alejandroaguiar@hotmail.com>
2017-10-08 20:50:41 -0300

    Level Spawner and Some obstacle modules

---
+++ CharacterControlerCasiFull/Assets/Scritps/Level/LevelSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour {
+
+    public List<Module> spawnModules;
+    public float[] spawners;
+    public float levelHeight;
+
+	void Start () {
+        spawners = new float[2] { transform.position.y, transform.position.y };       
+        SpawnLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //0 left spawner , 1 right spawner the match the spawners list
+    void setModule( int aPos)
+    {
+        int randomModule = Random.Range(0, spawnModules.Count);
+        Vector3 positionToSpawn = new Vector3(transform.position.x, spawners[aPos]);
+        Module newModule = Instantiate(spawnModules[randomModule],positionToSpawn,Quaternion.identity,transform);
+        if (aPos == 1)
+            newModule.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        spawners[aPos] += newModule.getHeight();
+    }
+
+    void SpawnLevel()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            while (spawners[i] < levelHeight)
+            {
+                setModule(i);
+            }
+        }
+    }
+}
---
+++ CharacterControlerCasiFull/Assets/Scritps/Level/Module.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module : MonoBehaviour {
+
+    private float height;
+	void Awake () {
+        height = GetComponent<BoxCollider>().size.y;
+	}
+	
+	public float getHeight()
+    {
+        return height;
+    }
+}
